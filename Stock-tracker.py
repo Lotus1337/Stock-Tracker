d79b3ccd2da65540b449e2b74c9d8f6a67e75bea
Lotus1337 <75097219+Lotus1337@users.noreply.github.com>
2020-12-31 09:26:52 -0600

    Update Stock-tracker.py

--- Stock-tracker.py
+++ Stock-tracker.py
@@ -1,7 +1,20 @@
+#Stock Tracker 
+
+#A stock tracker for your stock of choice
+
+#How to use:
+#Find the URL of the yahoo page of your Stock
+#Switch it out with the URL already in the code
+#Run the code
+
+#If any problems please comment on the github page
+
 def StockTracker():
 
     import requests
     from bs4 import BeautifulSoup
+    
+    #Just switch out the URL with yours here
 
     url = input("Enter the yahoo finance URL of the stock you are looking for: ")
 
